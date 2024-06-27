825640a8e07c8b58fc35d9f5647aeaaf3789b69d
pobe22 <benjaminpollak22@gmail.com>
2024-06-27 11:53:55 +0200

    add cardgenerator functionality

--- js/cardGenerator.js
+++ js/cardGenerator.js
@@ -1,41 +1,56 @@
+// Funktion zum Laden der JSON-Daten
+async function loadConfig() {
+    try {
+        const response = await fetch('/json/content.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+}
 
-// function createCards(listData) {
-//     const container = document.createElement('div');
-//     container.className = 'container';
-//     const row = document.createElement('div');
-//     row.className = 'row';
-//     container.appendChild(row);
-  
-//     Object.keys(listData).forEach(key => {
-//       const section = listData[key];
-//       const col = document.createElement('div');
-//       col.className = 'col-md-4';
-//       const card = document.createElement('div');
-//       card.className = 'card';
-//       const cardBody = document.createElement('div');
-//       cardBody.className = 'card-body';
-  
-//       const cardTitle = document.createElement('h5');
-//       cardTitle.className = 'card-title';
-//       cardTitle.textContent = section.headline;
-  
-//       const cardText = document.createElement('p');
-//       cardText.className = 'card-text';
-//       cardText.textContent = section.text;
-  
-//       cardBody.appendChild(cardTitle);
-//       cardBody.appendChild(cardText);
-//       card.appendChild(cardBody);
-//       col.appendChild(card);
-//       row.appendChild(col);
-//     });
-  
-//     document.body.appendChild(container);
-//   }
-  
-//   document.addEventListener('DOMContentLoaded', async () => {
-//     const config = await loadConfig();
-//     if (config.home && config.home.list_data) {
-//       createCards(config.home.list_data);
-//     }
-//   });
+// Funktion zum Erstellen der Karten
+function createCards(listData) {
+    const container = document.createElement('div');
+    container.className = 'container';
+    const row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+
+    Object.keys(listData).forEach(key => {
+        const section = listData[key];
+        const col = document.createElement('div');
+        col.className = 'col-md-4';
+        const card = document.createElement('div');
+        card.className = 'card';
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = section.headline;
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = section.text;
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        card.appendChild(cardBody);
+        col.appendChild(card);
+        row.appendChild(col);
+    });
+
+    const contentContainer = document.getElementById('content-container-home');
+    contentContainer.appendChild(container);
+}
+
+// DOMContentLoaded Eventlistener, um die Karten zu erstellen
+document.addEventListener('DOMContentLoaded', async () => {
+    const data = await loadConfig();
+    if (data && data.siteSettings && data.siteSettings.pages && data.siteSettings.pages.home && data.siteSettings.pages.home.list_data) {
+        createCards(data.siteSettings.pages.home.list_data);
+    }
+});
--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", loadButton);
 
-function loadButton(){
+function loadButton() {
     var tag = document.createElement("button");
     var text = document.createTextNode("Farbe ändern");
     tag.addEventListener("click", changeColor);
@@ -12,14 +12,27 @@ function loadButton(){
     tag.style.zIndex = "1000";
 
     document.body.appendChild(tag);
+
+    // Beim Laden der Seite prüfen, ob eine gespeicherte Farbe vorhanden ist und setzen
+    var storedColor = localStorage.getItem("selectedColor");
+    if (storedColor) {
+        var root = document.documentElement;
+        root.style.setProperty("--main-color1", storedColor);
+    }
 }
 
 function changeColor() {
     var root = document.documentElement;
- 
-    // Get the current background color
+
+    // Finde jetzige Farbe heraus
     var currentColor = getComputedStyle(root).getPropertyValue('--main-color1').trim();
- 
-    // Check current color and toggle to a new one
-    root.style.setProperty("--main-color1", currentColor === "#ADCBC7" ? "#79A6A0" : "#ADCBC7");
+
+    // Zwischen zwei Farben wechseln
+    var newColor = currentColor === "#ADCBC7" ? "#79A6A0" : "#ADCBC7";
+    
+    // Neue Farbe setzen
+    root.style.setProperty("--main-color1", newColor);
+
+    // Speichere Farbe
+    localStorage.setItem("selectedColor", newColor);
 }
